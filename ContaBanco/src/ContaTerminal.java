dcb05b0496b139bf29d01c2ea4fd4034fd44ec80
rjprattes <85262485+rjprattes@users.noreply.github.com>
2024-06-19 12:14:22 -0300

    Desafio poo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        Double saldo = 235.81;
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            Double saldo = 235.81;
 
-        System.out.println("Por favor, digite o número da agência: ");
-        String agencia = scanner.next();
+            System.out.println("Por favor, digite o número da agência: ");
+            String agencia = scanner.next();
 
-        System.out.println("Agora digite o número da conta: ");
-        int numeroConta = scanner.nextInt();
+            System.out.println("Agora digite o número da conta: ");
+            int numeroConta = scanner.nextInt();
 
-        System.out.println("Olá Rogeres, seja bem vindo ao nosso banco, sua agencia é "+ agencia + ", conta " + numeroConta + ", seu saldo atual é de " + saldo);
-       
+            System.out.println("Olá Rogeres, seja bem vindo ao nosso banco, sua agencia é "+ agencia + ", conta " + numeroConta + ", seu saldo atual é de " + saldo);
+        }
     }
 }
